b02a66c49bf2c9878eea37eec7cc95d30685201d
unknown <coolshiv0721@gmail.com>
2022-10-12 18:19:03 +0530

    Created post submission

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import PostCreate from './PostCreate';
 
 const App = () => {
   return (
-    <div>Blog App</div>
-  )
-}
+    <div className='container'>
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
+};
 
-export default App
+export default App;
