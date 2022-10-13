1694532edd97ecfa01c19b7e0e875d0f0d2148c9
unknown <coolshiv0721@gmail.com>
2022-10-13 15:33:45 +0530

    Emitting comment creation events

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,13 +14,26 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { id } = req.params;
   const { content } = req.body;
+
   const comments = commentsByPostId[id] || [];
+
   comments.push({ id: commentId, content });
+
   commentsByPostId[id] = comments;
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: id,
+    },
+  });
+
   res.status(201).send(comments);
 });
 
